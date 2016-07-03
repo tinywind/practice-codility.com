162433dd80a3916ddab8745c93d75742e89df82b
tinywind <tinywind0@gmail.com>
2016-07-04 00:07:23 +0900

    cpp HelloWorld

---
+++ cpp/Demo.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	p();
+	return 0;
+}
+
+void p() {
+	cout << "Hello World!!" << endl;
+}
+
+int solution(vector<int> &A) {
+	return 0;
+}
