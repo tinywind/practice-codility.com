30acab8ae9b3ef373d652f13c16e0ffa78c56512
tinywind <tinywind0@gmail.com>
2016-07-04 00:39:14 +0900

    cpp demo commit

--- cpp/Demo.cpp
+++ cpp/Demo.cpp
@@ -1,16 +1,45 @@
-#include <algorithm>
+#include <iostream>
 #include <vector>
 using namespace std;
 
-int main() {
-	p();
-	return 0;
+int solution(vector<int> &A) {
+    long lastLeft = 0L;
+    long lastRight = 0L;
+    int lastP = -1;
+    for (int p = 0; p < A.size(); p++) {
+        long left = 0L;
+        long right = 0L;
+
+        if (lastP <= 0)
+            for (int i = 0; i < p; i++)
+                left += A[i];
+        else
+            left = lastLeft + A[lastP];
+
+        if (lastP < 0)
+            for (int i = p + 1; i < A.size(); i++)
+                right += A[i];
+        else if (lastP + 1 < A.size())
+            right = lastRight - A[lastP + 1];
+        else
+            right = 0L;
+
+        if (left == right)
+            return p;
+
+        lastP = p;
+        lastLeft = left;
+        lastRight = right;
+    }
+
+    return -1;
 }
 
 void p() {
-	cout << "Hello World!!" << endl;
+	cout << "!!!Hello World!!!" << endl;
 }
 
-int solution(vector<int> &A) {
+int main() {
+	p();
 	return 0;
 }
