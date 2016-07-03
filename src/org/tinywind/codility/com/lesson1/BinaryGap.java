f5d8c2143e8d04e656f085dab0cacf93a42dbe18
tinywind <tinywind0@gmail.com>
2016-07-03 23:24:20 +0900

    lesson1.BinaryGap

---
+++ src/org/tinywind/codility/com/lesson1/BinaryGap.java
@@ -0,0 +1,26 @@
+package org.tinywind.codility.com.lesson1;
+
+public class BinaryGap {
+    public static void main(String[] args) {
+        p(1);
+        p(1000);
+    }
+
+    private static void p(int N) {
+        System.out.println(N + " : " + solution(N));
+    }
+
+    private static int solution(int N) {
+        int result = 0;
+        String s = Integer.toBinaryString(N);
+        int iStart = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                int length = i - iStart - 1;
+                if (length > result) result = length;
+                iStart = i;
+            }
+        }
+        return result;
+    }
+}
